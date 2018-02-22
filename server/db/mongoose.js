25a03531ae96f30805487ef90f5f2228c4ec4d37
Wang <wange@ad.tri.sbc.com>
2018-02-21 22:06:11 -0800

    Setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,23 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = { mongoose };
+
+// var mongoose = require('mongoose');
+
+// //copy the link from "To connect using a driver via the standard MongoDB URI" section
+// //insert db user name and password here
+// const REMOTE_MONGO = 'mongodb://wange:wange@ds243728.mlab.com:43728/node1993';
+// const LOCAL_MONGO = 'mongodb://localhost:27017/TodoApp';
+// const MONGO_URI = process.env.PORT ? REMOTE_MONGO : LOCAL_MONGO;
+
+// mongoose.Promise = global.Promise;
+// mongoose.connect(MONGO_URI).then(() => {
+//     console.log('Connected to Mongo instance.')
+// }, (err) => {
+//     console.log('Error connecting to Mongo instance: ', err);
+// });
+
+// module.export = { mongoose };

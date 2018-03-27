54ae4f61fc2a691ba85519d9ba2fc1f144ae6881
Wang <changwang.tracy@gmail.com>
2018-03-27 09:54:00 -0700

    Add tests for GET /users/me and POST /users

--- server/models/user.js
+++ server/models/user.js
@@ -71,7 +71,6 @@ UserSchema.pre('save', function(next){
     var user = this;
     
     if(user.isModified('password')) {
-        var password = user.password;
         bcrypt.genSalt(10, (err, salt) => {
             bcrypt.hash(user.password, salt, (err, hash)=> {
                 user.password = hash;
